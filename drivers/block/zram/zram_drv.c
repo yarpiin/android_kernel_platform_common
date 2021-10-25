42919d5f1dfb6e8fe27dd0735a96f4a78006675f
Ming Lei <ming.lei@redhat.com>
2021-10-25 10:54:26 +0800

    zram: replace fsync_bdev with sync_blockdev
    When calling fsync_bdev(), zram driver guarantees that the bdev won't be
    opened by anyone, then there can't be one active fs/superblock over the
    zram bdev, so replace fsync_bdev with sync_blockdev.
    
    Reviewed-by: Luis Chamberlain <mcgrof@kernel.org>
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Link: https://lore.kernel.org/r/20211025025426.2815424-5-ming.lei@redhat.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1808,7 +1808,7 @@ static ssize_t reset_store(struct device *dev,
 	mutex_unlock(&bdev->bd_mutex);
 
 	/* Make sure all the pending I/O are finished */
-	fsync_bdev(bdev);
+	sync_blockdev(bdev);
 	zram_reset_device(zram);
 	revalidate_disk_size(zram->disk, true);
 	bdput(bdev);
@@ -2016,7 +2016,7 @@ static int zram_remove(struct zram *zram)
 	zram_debugfs_unregister(zram);
 
 	/* Make sure all the pending I/O are finished */
-	fsync_bdev(bdev);
+	sync_blockdev(bdev);
 	zram_reset_device(zram);
 	bdput(bdev);
 
